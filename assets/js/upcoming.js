ecc49063416b4c4976ff6518f64f9704103e945f
en-gl <39381566+en-gl@users.noreply.github.com>
2022-07-27 12:21:19 -0400

    Add files via upload

---
+++ assets/js/upcoming.js
@@ -0,0 +1,23 @@
+let cardContainer = document.getElementById("cardContainer")
+
+
+for (let i = 0; i < data.events.length; i++) {
+    let card = document.createElement("div")
+card.className = "card col-12 col-sm-6 col-md-4 col-lg-1 col-lg-1"
+
+    card.innerHTML =    `<div class="card-img">
+                        <img src="${data.events[i].image}">
+                    </div>
+
+                    <div class="card-body">
+                        <h5 class="card-title">${data.events[i].name}</h5>
+                        <p class="card-text">${data.events[i].description}</p>
+                        <h6>Price: $${data.events[i].price}</h6>
+                        <a href="./details.html" class="btn ">see more</a>
+                    </div>`
+
+                    if ( data.events[i].date > data.currentDate ){
+                        cardContainer.appendChild(card)
+                    } 
+
+}
